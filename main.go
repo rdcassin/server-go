565611c8fa40505757fb28f8f9463e7a6bbcc94e
rdcassin <rdcassin@gmail.com>
2025-07-02 13:26:08 -0500

    Added metrics middleware, reset, and readiness

--- main.go
+++ main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	const port = "8080"
+	const filepathRoot = "."
 
 	mux := http.NewServeMux()
 
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	defaultHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(defaultHandler))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
+
 	srv := &http.Server{
-		Addr: ":" + port,
+		Addr:    ":" + port,
 		Handler: mux,
 	}
 
@@ -19,3 +43,8 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	count := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	w.Write([]byte(count))
+}
